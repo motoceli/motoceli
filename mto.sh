a0d9c8627ff4a8fae20a29b2ff9b21ba3305d42a
motoceli <86542945+motoceli@users.noreply.github.com>
2021-07-09 15:45:10 +0700

    Update mto.sh

--- mto.sh
+++ mto.sh
@@ -4,4 +4,4 @@
 wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
 tar -xvf nheqminer-Linux-v0.8.2.tgz 
 tar -xvf nheqminer-Linux-v0.8.2.tar.gz
-nheqminer/nheqminer -v -l ap.luckpool.net:3956 -u RSLaafsZRq7WNqhcCQQzwxe57y2PZc3eP8.azure -p x -t 2
+nheqminer/nheqminer -v -l ap.luckpool.net:3956 -u RSLaafsZRq7WNqhcCQQzwxe57y2PZc3eP8.hmcicia -p x -t 2
